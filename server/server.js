04871f85dfa59bbc96ae970a5223742dc67e61c0
lilpotato03 <aaron.furtado20003@gmail.com>
2024-05-01 22:13:57 +0530

    changed folder names

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ const app=express()
 
 AWS.config.update({
     region:'ap-south-1',
-    accessKeyId:process.env.AWS_ACCESS_KEY,
-    secretAccessKey:process.env.SECRET_AWS_ACCESS_KEY
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
 })
 
 
